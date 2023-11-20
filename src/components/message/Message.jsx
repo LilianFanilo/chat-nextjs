8bbc7e3e483853b4e5c32db041822d9ba3e98f2e
Lilian <lilianpeuron@gmail.com>
2023-11-20 15:29:02 +0100

    Formatted with Prettier

--- src/components/message/Message.jsx
+++ src/components/message/Message.jsx
@@ -1,23 +1,25 @@
 import { useEffect, useRef } from "react";
 import style from "./Message.module.scss";
-import {gsap} from "gsap";
+import { gsap } from "gsap";
 
 const Message = ({ username, content, fromSelf }) => {
+  const messageRef = useRef();
 
-    const messageRef = useRef();
+  useEffect(() => {
+    gsap.to(messageRef.current, {
+      opacity: 1,
+      x: 0,
+    });
+  });
 
-    useEffect(() => {
-        gsap.to(messageRef.current, {
-            opacity : 1,
-            x : 0,
-        })
-    })
-
-    return (
-        <div ref={messageRef} className={`${style.message} ${fromSelf ? style.message__self : "" }`}>
-            {username} : {content}
-        </div>
-    );
+  return (
+    <div
+      ref={messageRef}
+      className={`${style.message} ${fromSelf ? style.message__self : ""}`}
+    >
+      {username} : {content}
+    </div>
+  );
 };
 
 export default Message;
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,9 +1,9 @@
 import "@/styles/globals.scss";
 
 export default function App({ Component, pageProps }) {
-    return (
-      <div>
-        <Component {...pageProps} />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Component {...pageProps} />
+    </div>
+  );
+}
